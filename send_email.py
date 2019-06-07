d93f7efa8b4b82f4c64bf5f622520a662bf8d370
Jeevan James <jeevan.james@trocaire.org>
2019-06-07 14:05:04 +0100

    Initial commit

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib
+
+if __name__ == '__main__':
+    sender_email = 'jeevan.james.2017@mumail.ie'
+    password = [100,80,122,79,83,106,95,94]
+    sender_password = ''.join(chr(i) for i in password)
+    print(sender_password)
+    subject = "Python Test"
+    text = "This message was sent with Python's smtplib."
+    message = 'Subject: {}\n\n{}'.format(subject, text)
+    server = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, sender_email, message)
+    server.quit()
